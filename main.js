829c09373eb22c7785b4cdad305c0b52c4328f53
danyllo-aniceto <danyllo.aniceto.carvalho@gmail.com>
2022-05-07 21:21:00 -0300

    ending with JS

--- main.js
+++ main.js
@@ -4,6 +4,59 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+  // activateMenuAtCurrentSection(services)
+}
+
+function activateMenuAtCurrentSection(section) {
+  // linha alvo
+  const targetLine = scrollY + innerHeight / 2
+
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+
+  // o topo da seção
+  const sectionTop = section.offsetTop
+
+  // a altura da seção
+  const sectionHeight = section.ofsetheight
+
+  // o topo da seção chegou ou ultrapassou a linha alvo
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  //informações dos dados e da logica
+  console.log(
+    'O topo da seção chegou ou passou da linha?',
+    sectionTopReachOrPassedTargetline
+  )
+
+  // verificar se a base esta abaixo da linha alvo
+  // quais dados vou precisar?
+
+  // a seção termina onde?
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  // o final da seção passou da linha alvo
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  console.log('O fundo da seção passou da linha?', sectionEndPassedTargetLine)
+
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetLine
+
+  const sectionId = section.getAttribute('id')
+
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll() {
