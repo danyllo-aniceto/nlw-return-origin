a4c2f56df8423f5a1efb09871e9e8eedf7baa811
danyllo-aniceto <danyllo.aniceto.carvalho@gmail.com>
2022-05-07 21:56:17 -0300

    correcting erros part2

--- main.js
+++ main.js
@@ -9,48 +9,27 @@ function onScroll() {
   activateMenuAtCurrentSection(services)
   activateMenuAtCurrentSection(about)
   activateMenuAtCurrentSection(contact)
-  // activateMenuAtCurrentSection(services)
 }
 
 function activateMenuAtCurrentSection(section) {
-  // linha alvo
   const targetLine = scrollY + innerHeight / 2
 
   // verificar se a seção passou da linha
   // quais dados vou precisar?
-
-  // o topo da seção
   const sectionTop = section.offsetTop
-
-  // a altura da seção
-  const sectionHeight = section.ofsetheight
-
-  // o topo da seção chegou ou ultrapassou a linha alvo
+  const sectionHeight = section.offsetHeight
   const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
 
-  //informações dos dados e da logica
-  console.log(
-    'O topo da seção chegou ou passou da linha?',
-    sectionTopReachOrPassedTargetline
-  )
+  // verificar se a base está abaixo da linha alvo
 
-  // verificar se a base esta abaixo da linha alvo
-  // quais dados vou precisar?
-
-  // a seção termina onde?
   const sectionEndsAt = sectionTop + sectionHeight
-
-  // o final da seção passou da linha alvo
-  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
-
-  console.log('O fundo da seção passou da linha?', sectionEndPassedTargetLine)
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
 
   // limites da seção
   const sectionBoundaries =
-    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetLine
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
 
   const sectionId = section.getAttribute('id')
-
   const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
 
   menuElement.classList.remove('active')
@@ -88,12 +67,12 @@ ScrollReveal({
   distance: '30px',
   duration: 700
 }).reveal(`
-#home, 
-#home img, 
-#home .stats, 
-#services,
-#services header,
-#services .card,
-#about,
-#about header,
-#about .content`)
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card
+  #about, 
+  #about header, 
+  #about .content`)
